719e40316f7085457ea95001d32ca00689735f28
Shubbu03 <thatcoderguyshubham@gmail.com>
2024-07-25 12:02:02 +0530

    refactor: Remove commented out code and optimize Notes component

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,14 +1,20 @@
-import { fakeData as notes } from "../assets/fakeData";
-import NoteCard from "../components/NoteCard";
+// import { fakeData as notes } from "../assets/fakeData";
 
-function Notes() {
+import NoteCard from "../components/NoteCard";
+import { useContext } from "react";
+import { NoteContext } from "../context/NoteContext";
+import Controls from "../components/Controls";
+
+const Notes = () => {
+  const { notes, setNotes } = useContext(NoteContext);
+
   return (
     <div>
       {notes.map((note) => (
-        <NoteCard note={note} key={note.$id} />
+        <NoteCard note={note} key={note.$id} setNotes={setNotes} />
       ))}
+      <Controls />
     </div>
   );
-}
-
+};
 export default Notes;
